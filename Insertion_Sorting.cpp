973db1b53bf3ad7f9fb9c6c49bb8ad7fd7186fc6
kunal <kunalkakati2003@gmail.com>
2021-12-12 22:09:39 +0530

    complete insrtion sort

--- Insertion_Sorting.cpp
+++ Insertion_Sorting.cpp
@@ -1,30 +1,39 @@
 #include<iostream>
 using namespace std;
 
+
+void DisplayArray(int *A, int size){
+    for (int i = 0; i < size; i++)
+    {
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void Insertion_Sort(int *A, int size){
+    int elem,j;
+    for (int i = 1; i <  size; i++)
+    {
+        elem = A[i];
+        j = i-1;
+        while (j >= 0 && A[j] > elem)
+        {
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = elem;
+    }
+    
+}
+
 int main(){
     int arr[] = {2,5,1,3,8,54,3,57,21};
     int size = sizeof(arr)/sizeof(int);
-
-    for (int i = 0; i < size-1; i++)
-    {
-        for (int j = i + 1; j < 0; j++)
-        {
-            if(arr[i] > arr[j]){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        
-    }
-
-    for (int i = 0; i < size; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
+    
+    DisplayArray(arr,size);
+    Insertion_Sort(arr,size);
+    DisplayArray(arr,size);
     
     
-
-
     return 0;
 }
