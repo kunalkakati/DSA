38e140f1db22cb913c4855bf75a89f41423021a8
kunal <kunalkakati2003@gmail.com>
2022-01-14 22:09:48 +0530

    Binary tree Traversals (PreOrder, PostOrder, InOrder)

--- Binary_Tree_Traversals.cpp
+++ Binary_Tree_Traversals.cpp
@@ -1,15 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public:
-    Node * left;
+class Node
+{
+public:
+    Node *left;
     int data;
-    Node * right;
+    Node *right;
 };
 
-Node * CreateNode(int data){
-    Node * New_Node = new Node;
+Node *CreateNode(int data)
+{
+    Node *New_Node = new Node;
     New_Node->data = data;
     New_Node->left = nullptr;
     New_Node->right = nullptr;
@@ -18,39 +20,46 @@ Node * CreateNode(int data){
 
 // Traversal: Preorder, Postorder, Inorder.
 
-//Preorder: root->left->right.
-void PreOrder(Node * root){
-    if(root != nullptr){
-        cout<<root->data<<" "; //*diff
+// Preorder: root->left->right.
+void PreOrder(Node *root)
+{
+    if (root != nullptr)
+    {
+        cout << root->data << " "; //*diff
         PreOrder(root->left);
         PreOrder(root->right);
     }
 }
 
 // Postorder: left->right->root.
-void PostOrder(Node * root){
-    if(root != nullptr){
+void PostOrder(Node *root)
+{
+    if (root != nullptr)
+    {
         PostOrder(root->left);
         PostOrder(root->right);
-        cout<<root->data<<" "; //*diff
+        cout << root->data << " "; //*diff
     }
 }
 
-//Inorder: left->root->right.
-void InOrder(Node * root){
-    if(root != nullptr){
+// Inorder: left->root->right.
+void InOrder(Node *root)
+{
+    if (root != nullptr)
+    {
         InOrder(root->left);
-        cout<<root->data<<" "; //*diff
+        cout << root->data << " "; //*diff
         InOrder(root->right);
     }
 }
 
-int main(){
+int main()
+{
 
-    Node * root = CreateNode(20);
-    Node * n1 = CreateNode(5);
-    Node * n2 = CreateNode(15);
-    Node * n3 = CreateNode(2);
+    Node *root = CreateNode(20);
+    Node *n1 = CreateNode(5);
+    Node *n2 = CreateNode(15);
+    Node *n3 = CreateNode(2);
 
     root->left = n1;
     root->right = n2;
@@ -58,11 +67,10 @@ int main(){
     n1->left = n3;
 
     PreOrder(root);
-    cout<<endl;
+    cout << endl;
     PostOrder(root);
-    cout<<endl;
+    cout << endl;
     InOrder(root);
-
 
     return 0;
 }
